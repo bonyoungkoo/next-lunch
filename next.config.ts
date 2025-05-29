793247b5afce6c2ddeb744f336d13b7c915c63f6
bonyoungkoo <gby1202@gmail.com>
2025-05-29 11:08:19 +0900

    update basePath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/next-lunch",
-  output: "export",
 };
 
 export default nextConfig;
