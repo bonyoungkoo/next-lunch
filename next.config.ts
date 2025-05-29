6bd92810ef10b185d85674b0b26a26743a2112e1
bonyoungkoo <gby1202@gmail.com>
2025-05-29 11:05:21 +0900

    update basePath

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ["images.pexels.com"],
-  },
+  basePath: "/next-lunch",
+  output: "export",
 };
 
 export default nextConfig;
